f52ed97645c1c72098e17bdf83998401449e029a
chl49 <chl49@sfu.ca>
2022-10-01 15:04:54 -0700

    client time

--- server/models/event.js
+++ server/models/event.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const event = mongoose.Schema({
     title: {type: String, required: true},
     capacity: {type: Number, default: 2, required: true},
-    password: {type: String, required: true},
+    //password: {type: String, required: true},
     created: {
         type: Date,
         default: Date.now
