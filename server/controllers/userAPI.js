2cd16df5f88103ae4105eda94d509b373ae0d5b9
Mightyy <59546774+Mighty303@users.noreply.github.com>
2022-10-01 13:40:45 -0700

    bug fixes

--- server/controllers/userAPI.js
+++ server/controllers/userAPI.js
@@ -1,22 +1,9 @@
 const Event = require("../models/event");
-module.exports = class eventAPI {
+module.exports = class userAPI {
 
     // fetch all posts
     static async fetchAllEvents(req, res) {
-        //res.send('Hello from API');
-        try {
-            const posts = await Event.find()
-            if(!posts) throw new Error('No bucketListItems') 
-            /*
-            const sorted = posts.sort((a,b) => {
-                return new Date(a.date).getTime() - new Date(b.date).getTime()
-            })
-            */
-            //res.status(200).json(sorted)
-            res.status(200).json(posts)
-        } catch (err) {
-            res.status(500).json({message: error.message})
-        }
+        res.send('Hello from API');
     }
 
     // fetch post by ID
@@ -25,31 +12,14 @@ module.exports = class eventAPI {
     }
 
     static async postEvent(req, res) {
-        const newEvent = new Event(req.body)
-        try {
-            const posts = await newEvent.save()
-            if (!posts) throw new Error('Something went wrong saving the bucketListItem')
-            res.status(200).json(posts)
-        } catch (err) {
-            res.status(500).json({ message: error.message })
-        }
-        //res.send('Post new event');
+        res.send('Post new event');
     }
 
     static async patchEvent(req, res) {
-        //res.send('Update event');
-        const { id} = req.params
-        try {
-            const response = await Event.findByIdAndUpdate(id, req.body)
-            if (!response) throw Error('Something went wrong')
-            const updated = { ...response._doc, ...req.body }
-            res.status(200).json(updated)
-        } catch (error) {
-            res.status(500).json({ message: error.message })
-        }
+        res.send('Patch new event')
     }
 
-    static async deleteEvent(req, res) {
+    static async deleteUser(req, res) {
         Event.findByIdAndDelete(req.params.id)
         .then(result => {
             res.status(204).send(result);
