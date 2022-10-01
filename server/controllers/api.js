c5d5fa222c7aad84b9e77de31fa6a78fd411bbab
chl49 <chl49@sfu.ca>
2022-10-01 11:29:32 -0700

    first post

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -1,7 +1,22 @@
+const Event = require("../models/event");
 module.exports = class API {
+
     // fetch all posts
     static async fetchAllEvents(req, res) {
-        res.send('Hello from API');
+        //res.send('Hello from API');
+        try {
+            const posts = await Event.find()
+            if(!posts) throw new Error('No bucketListItems') 
+            /*
+            const sorted = posts.sort((a,b) => {
+                return new Date(a.date).getTime() - new Date(b.date).getTime()
+            })
+            */
+            //res.status(200).json(sorted)
+            res.status(200).json(posts)
+        } catch (err) {
+            res.status(500).json({message: error.message})
+        }
     }
 
     // fetch post by ID
@@ -10,7 +25,15 @@ module.exports = class API {
     }
 
     static async postEvent(req, res) {
-        res.send('Post new event');
+        const newEvent = new Event(req.body)
+        try {
+            const posts = await newEvent.save()
+            if (!posts) throw new Error('Something went wrong saving the bucketListItem')
+            res.status(200).json(posts)
+        } catch (err) {
+            res.status(500).json({ message: error.message })
+        }
+        //res.send('Post new event');
     }
 
     static async patchEvent(req, res) {
--- server/db/connection.js
+++ server/db/connection.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
-const user = process.env.USER;
-const pw = process.env.PW;
-const dbName = process.env.DB_NAME;
+//const user = process.env.USER;
+//const pw = process.env.PW;
+//const dbName = process.env.DB_NAME;
+const user ='martinwong303';
+const pw ='3weGHcxE0RV2m1EQ';
+const dbName = 'Hackathon'
 console.log(`${user} ${pw} ${dbName}`);
 let mongoDB = `mongodb+srv://${user}:${pw}@cluster0.icijb.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
